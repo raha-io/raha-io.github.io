826880446795d857975ee2adb41e17846a0a776d
Parham Alvani <parham.alvani@gmail.com>
2026-01-02 08:47:56 +0000

    chore: remove middlewares

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -4,7 +4,6 @@ import { defineRouting } from 'next-intl/routing';
 export const routing = defineRouting({
   locales: ['en', 'fa'],
   defaultLocale: 'en',
-  localeDetection: true,
 });
 
 export const { Link, redirect, usePathname, useRouter, getPathname } = createNavigation(routing);
